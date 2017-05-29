9a383ad58ca5053531e7f3980c003e324879545d
sgrunwald96 <sgrunwald96@gmail.com>
2017-05-29 15:24:21 -0700

    Update header. (Test commit.)

--- app/src/main/java/com/labgmail/pomona/greenberg/cnccarvingfactory/DrawingActivity.java
+++ app/src/main/java/com/labgmail/pomona/greenberg/cnccarvingfactory/DrawingActivity.java
@@ -9,8 +9,7 @@ import android.view.MotionEvent;
 import android.view.View;
 
 /**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
+ * Central drawing activity for (eventual) output to a CNC machine.
  */
 public class DrawingActivity extends AppCompatActivity {
     /**
