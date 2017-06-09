4560256e61e9dbbf198ba1ae569b7769dd14a0b7
Michael Greenberg <michael.m.greenberg@gmail.com>
2017-06-09 14:46:20 -0700

    migrate to modern preferencemanager, not the support version

--- app/src/main/java/com/labgmail/pomona/greenberg/cnccarvingfactory/DisplaySettingsActivity.java
+++ app/src/main/java/com/labgmail/pomona/greenberg/cnccarvingfactory/DisplaySettingsActivity.java
@@ -8,7 +8,6 @@ import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.preference.PreferenceManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
